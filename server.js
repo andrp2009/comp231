eca390e4af63a07bbbb704e19b34711f559050f1
Andrzej Popowski <prosysinfo@prosysinfo.com>
2018-12-07 11:08:50 -0500

    Adding SMS feature

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const pg = require('pg');
 const dotenv = require('dotenv');
+const Nexmo = require('nexmo');
 
 const pool = new pg.Pool();
 
@@ -28,11 +29,35 @@ pool.connect()
 });
 
 // Static files
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-// Chat test
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
+});
+
+// Chat test
+app.get('/chat', (req, res) => {
+	res.sendFile(__dirname + '/public/chat.html');
+});
+
+// SMS test
+app.get('/sms', (req, res) => {
+	res.sendFile(__dirname + '/public/sms.html');
+});
+
+
+app.get('/sendSms', (req, res) => {
+	const nexmo = new Nexmo({
+	  apiKey: 'f4ceb37c',
+	  apiSecret: 'q6yJXtRmT3VqpUyx'
+	});
+	
+	const from = '12262426276';
+	const to = req.query.number;
+	const text = req.query.message;
+	nexmo.message.sendSms(from, to, text);
+
+	res.sendFile(__dirname + '/public/sms.html');
 });
 
 // Socket connection
